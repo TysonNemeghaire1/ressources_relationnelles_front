a850ce9c9dfa1e9b49a9cc3dc417a35e30417743
ClaireH <clairehien.osmont@gmail.com>
2024-03-04 11:22:17 +0100

    light button

--- src/app/account/page.tsx
+++ src/app/account/page.tsx
@@ -1,9 +1,35 @@
-import React from 'react';
+'use client';
+
+import React, { useState } from 'react';
+import Button from "@/components/lightButton/lightButton";
 
 export default function Page() {
+    
+    const InfoEdit = () => {
+        console.log("MODIFIER MES INFORMATIONS button clicked");
+    };
+
+    const PasswordEdit = () => {
+        console.log("MODIFIER MON MOT DE PASSE button clicked");
+    };
+
     return (
-        <main className="flex min-h-screen flex-col items-center justify-center p-4 bg-gray-100">
-            MON COMPTE
-        </main>
+        <div className="flex justify-between">
+            <h1>MON COMPTE</h1>
+            <div>
+                <Button
+                    text={"MODIFIER MES INFORMATIONS"}
+                    onClick={InfoEdit}
+                    value={"na"}
+                    active={"na"}
+                />
+                <Button
+                    text={"MODIFIER MON MOT DE PASSE"}
+                    onClick={PasswordEdit} 
+                    value={"na"}
+                    active={"na"}
+                />
+            </div>
+        </div>
     );
 }
