abd0d8965b86ad00c4d7a6336dfb635df98f719a
Tyson <nemeghaire.tyson@outlook.be>
2024-03-04 11:44:00 +0100

    refactor(homepage):Update trendy ressources

--- src/components/cards/ressourceCard.tsx
+++ src/components/cards/ressourceCard.tsx
@@ -1,35 +1,49 @@
-'use client';
-import Link from 'next/link';
-import React from 'react';
+"use client";
+import Link from "next/link";
+import React from "react";
 
 interface DataProps {
-    id: string,
-    name: string;
-    category: string;
-    date: string;
-    author: string;
-    content: string;
+  id: string;
+  name: string;
+  category: string;
+  date: string;
+  author: string;
+  content: string;
 }
 
 interface RessourceCardProps {
-    data: DataProps;
-    isTrendy: boolean;
+  data: DataProps;
+  isTrendy: boolean;
 }
 
 const RessourceCard: React.FC<RessourceCardProps> = ({ data, isTrendy }) => {
-    return (
-        <Link href={`ressource/${data.id}`}>
-            <div className={`${isTrendy ? '' : 'flex flex-col gap-3'} w-full`}>
-                <div className={`${isTrendy ? 'flex' : 'flex flex-col gap-3'} justify-between`}>
-                    <p className={`${isTrendy ? 'w-1/2' : 'w-full'} text-black font-bold text-[20px]`}>{data.name}</p>
-                    <div className={`${isTrendy ? 'w-1/2' : 'w-full'} justify-end`}>
-                        <p className='text-black font-italic text-[15px]'>{`${data.category} - ${data.date} - ${data.author}`}</p>
-                    </div>
-                </div>
-                <p className='text-black font-light text-[15px] text-justify'>{data.content}</p>
-            </div>
-        </Link>
-    );
-}
+  const truncateString = (str: string, num: number) =>
+    str.length <= num ? str : str.slice(0, num) + "...";
+
+  return (
+    <Link href={`ressource/${data.id}`}>
+      <div className={`${isTrendy ? "" : "flex flex-col gap-3"} w-full p-2 hover:bg-custom-blue-0`}>
+        <div
+          className={`${isTrendy ? "flex" : "flex flex-col gap-3"} w-full justify-between`}
+        >
+          <p
+            className={`${isTrendy ? "w-1/2" : "w-full"} text-black font-bold text-lg`}
+          >
+            {data.name}
+          </p>
+          <div
+            className={`${isTrendy ? "w-1/2" : "w-full"} text-right italic font-light`}
+          >
+            <p>
+              <span className="font-bold">{data.category}</span> - {data.date} -{" "}
+              {data.author}
+            </p>
+          </div>
+        </div>
+        <p className="text-black font-light text-justify">{truncateString(data.content, 200)}</p>
+      </div>
+    </Link>
+  );
+};
 
 export default RessourceCard;
--- src/components/trendyRessources/index.tsx
+++ src/components/trendyRessources/index.tsx
@@ -2,15 +2,17 @@
 import React, { useState } from "react";
 import { FaRegShareFromSquare, FaRegStar } from "react-icons/fa6";
 import Button from "@/components/trendyRessources/button";
+import RessourceCard from "@/components/cards/ressourceCard";
 
 //TODO Changer le h2 en fonction du bouton
 
 export type filterType = "all" | "bookmarks" | "shared";
+
 function TrendyRessources() {
   const [filter, setFilter] = useState<filterType>("all");
   return (
     <main className="hover min-h-fit w-full lg:w-3/4 bg-trendy-ressource-gradient">
-      <div className="flex items-center justify-between mr-5 mt-5">
+      <div className="flex items-center justify-between md:mx-5 mt-5">
         <div className="flex gap-2">
           <Button
             text={"TOUTES"}
@@ -33,76 +35,131 @@ function TrendyRessources() {
             active={filter}
           />
         </div>
-        <h2 className="xl:text-2xl lg:text-lg  hidden md:inline font-bold text-white">RESSOURCES À LA UNE</h2>
+        <h2 className="xl:text-2xl lg:text-lg  hidden md:inline font-bold text-white">
+          RESSOURCES À LA UNE
+        </h2>
       </div>
-      <div className="xl:w-11/12 mx-auto my-5 h-80 overflow-y-auto">
-        <p>test</p>
-        <p>test</p>
-        <p>test</p>
-        <p>test</p>
-        <p>test</p>
-        <p>test</p>
-        <p>test</p>     <p>test</p>
-        <p>test</p>
-        <p>test</p>
-        <p>test</p>
-        <p>test</p>
-        <p>test</p>
-        <p>test</p>     <p>test</p>
-        <p>test</p>
-        <p>test</p>
-        <p>test</p>
-        <p>test</p>
-        <p>test</p>
-        <p>test</p>     <p>test</p>
-        <p>test</p>
-        <p>test</p>
-        <p>test</p>
-        <p>test</p>
-        <p>test</p>
-        <p>test</p>     <p>test</p>
-        <p>test</p>
-        <p>test</p>
-        <p>test</p>
-        <p>test</p>
-        <p>test</p>
-        <p>test</p>     <p>test</p>
-        <p>test</p>
-        <p>test</p>
-        <p>test</p>
-        <p>test</p>
-        <p>test</p>
-        <p>test</p>     <p>test</p>
-        <p>test</p>
-        <p>test</p>
-        <p>test</p>
-        <p>test</p>
-        <p>test</p>
-        <p>test</p>     <p>test</p>
-        <p>test</p>
-        <p>test</p>
-        <p>test</p>
-        <p>test</p>
-        <p>test</p>
-        <p>test</p>     <p>test</p>
-        <p>test</p>
-        <p>test</p>
-        <p>test</p>
-        <p>test</p>
-        <p>test</p>
-        <p>test</p>     <p>test</p>
-        <p>test</p>
-        <p>test</p>
-        <p>test</p>
-        <p>test</p>
-        <p>test</p>
-        <p>test</p>     <p>test</p>
-        <p>test</p>
-        <p>test</p>
-        <p>test</p>
-        <p>test</p>
-        <p>test</p>
-        <p>test</p>
+      <div className="xl:w-11/12 mx-auto my-5 h-80 overflow-y-auto flex flex-col gap-5 pr-3 scroll-bar">
+        <RessourceCard
+          data={{
+            id: "12",
+            name: "toto",
+            author: "cards",
+            category: "1a2e05",
+            content:
+              "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum",
+            date: "12/09/2012",
+          }}
+          isTrendy={true}
+        />
+        <RessourceCard
+          data={{
+            id: "12",
+            name: "toto",
+            author: "cards",
+            category: "1a2e05",
+            content:
+              "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum",
+            date: "12/09/2012",
+          }}
+          isTrendy={true}
+        />
+        <RessourceCard
+          data={{
+            id: "12",
+            name: "toto",
+            author: "cards",
+            category: "1a2e05",
+            content:
+              "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum",
+            date: "12/09/2012",
+          }}
+          isTrendy={true}
+        />
+        <RessourceCard
+          data={{
+            id: "12",
+            name: "toto",
+            author: "cards",
+            category: "1a2e05",
+            content:
+              "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum",
+            date: "12/09/2012",
+          }}
+          isTrendy={true}
+        />
+        <RessourceCard
+          data={{
+            id: "12",
+            name: "toto",
+            author: "cards",
+            category: "1a2e05",
+            content:
+              "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum",
+            date: "12/09/2012",
+          }}
+          isTrendy={true}
+        />
+        <RessourceCard
+          data={{
+            id: "12",
+            name: "toto",
+            author: "cards",
+            category: "1a2e05",
+            content:
+              "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum",
+            date: "12/09/2012",
+          }}
+          isTrendy={true}
+        />
+        <RessourceCard
+          data={{
+            id: "12",
+            name: "toto",
+            author: "cards",
+            category: "1a2e05",
+            content:
+              "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum",
+            date: "12/09/2012",
+          }}
+          isTrendy={true}
+        />
+        <RessourceCard
+          data={{
+            id: "12",
+            name: "toto",
+            author: "cards",
+            category: "1a2e05",
+            content:
+              "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum",
+            date: "12/09/2012",
+          }}
+          isTrendy={true}
+        />
+        <RessourceCard
+          data={{
+            id: "12",
+            name: "toto",
+            author: "cards",
+            category: "1a2e05",
+            content:
+              "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum",
+            date: "12/09/2012",
+          }}
+          isTrendy={true}
+        />
+        <RessourceCard
+          data={{
+            id: "12",
+            name: "toto",
+            author: "cards",
+            category: "1a2e05",
+            content:
+              "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum",
+            date: "12/09/2012",
+          }}
+          isTrendy={true}
+        />
       </div>
     </main>
   );
