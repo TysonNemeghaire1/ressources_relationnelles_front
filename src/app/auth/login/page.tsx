76184e5079ca6ec8291b8eabc763053853fa3dcd
Septimania <154997503+Septimania@users.noreply.github.com>
2024-03-05 14:47:00 +0100

    fix(loginPage):Fix login and register page

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -5,12 +5,12 @@ import Image from "next/image";
 export default function Login() {
 
   return (
-    <main>
-      <h1 className="text-4xl text-gray-700 text-center font-light mb-4 align-top pt-[5vh]">CONNEXION</h1>
-      <div className="flex flex-col items-center justify-center p-4 white">
+    <main className="pb-20">
+      <h1 className="text-4xl text-gray-700 text-center font-light mb-4 align-top pt-20">CONNEXION</h1>
+      <div className="flex flex-col items-center justify-center p-4 white h-full w-full">
         <form className="space-y-4">
-          <div className="flex flex-col md:flex-row gap-2 auto-rows-auto">
-            <div className='flex items-center justify-center h-[30vh]  md:h-[40vh] w-[40vh]'>
+          <div className="flex flex-col md:flex-row gap-2 auto-rows-auto md:h-full w-full">
+            <div className='flex items-center justify-center h-1/3 w-auto  md:py-10 w-full'>
               <div>
                 <label htmlFor="email" className="block text-sm font-medium text-gray-700">Adresse mail</label>
                 <input type="email" id="email" className="mt-1 p-2 w-full border-2 border-gray-300 rounded-md shadow-sm focus:border-indigo-500 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 text-gray-700" placeholder="mail@exemple.com" required />
@@ -28,22 +28,22 @@ export default function Login() {
                 </div>
               </div>
             </div>
-            <div className='flex items-center justify-center h-[10vh] w-[40vh]  md:h-[40vh] w-[20vh]'>
+            <div className='flex items-center justify-center py-10 w-auto  md:py-10 px-10'>
               <div className='bg-blue-500 flex justify-center items-center md:h-full w-px  max-sm:w-full h-px'>
                 <div className='bg-white z-10 text-2xl text-center max-sm:w-2/12'>
                   OU
                 </div>
               </div>
             </div>
-            <div className='flex items-center justify-center h-[10vh] w-full  md:h-[40vh] w-[40vh]'>
+            <div className='flex items-center justify-center h-1/3 w-auto  md:py-24 w-full'>
               <div>
-              <Image
-                width={240}
-                height={68}
-                src="/FranceConnect.png"
-                alt="France Connect"
-              />
-              <a href='https://franceconnect.gouv.fr/'>Qu'est-ce que FranceConnect ?</a>
+                <Image
+                  width={240}
+                  height={68}
+                  src="/FranceConnect.png"
+                  alt="France Connect"
+                />
+                <a href='https://franceconnect.gouv.fr/'>Qu'est-ce que FranceConnect ?</a>
               </div>
             </div>
           </div>
