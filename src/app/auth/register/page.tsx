e0519e572c9bb9727d34c3c03be71b0cf7e7abcc
Septimania <154997503+Septimania@users.noreply.github.com>
2024-03-05 14:36:29 +0100

    Add files via upload

--- src/app/auth/register/page.tsx
+++ src/app/auth/register/page.tsx
@@ -3,62 +3,36 @@ import Link from 'next/link';
 
 export default function Register() {
   return (
-    // <main className="flex min-h-screen flex-col items-center justify-center p-4 bg-gray-100">
-    //   <div className="p-8 bg-white shadow-md rounded-lg w-full max-w-md">
-    //     <form className="space-y-4">
-    //     <h1 className="text-2xl font-bold text-gray-700 text-center mb-4">INSCRIPTION</h1>
-    //       <div>
-    //         <label htmlFor="email" className="block text-sm font-medium text-gray-700">Adresse mail</label>
-    //         <input type="email" id="email" className="mt-1 p-2 w-full border-2 border-gray-300 rounded-md shadow-sm focus:border-indigo-500 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 text-gray-700" placeholder="mail@exemple.com" required />
-    //       </div>
-    //       <div>
-    //         <label htmlFor="password" className="block text-sm font-medium text-gray-700">Mot de passe</label>
-    //         <input type="password" id="password" className="mt-1 p-2 w-full border-2 border-gray-300 rounded-md shadow-sm focus:border-indigo-500 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 text-gray-700" required />
-    //       </div>
-    //       <div>
-    //         <label htmlFor="confirm-password" className="block text-sm font-medium text-gray-700">Confirmez le mot de passe</label>
-    //         <input type="password" id="confirm-password" className="mt-1 p-2 w-full border-2 border-gray-300 rounded-md shadow-sm focus:border-indigo-500 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 text-gray-700" required />
-    //       </div>
-    //       <div className="flex items-center justify-between">
-    //         <button type="submit" className="px-4 py-2 bg-indigo-600 text-white rounded-md hover:bg-indigo-700 focus:outline-none focus:bg-indigo-700">S'inscrire</button>
-    //         <Link href={'/auth/login'}>
-    //           <div className="text-sm text-indigo-600 hover:underline">Déjà un compte ? Se connecter</div>
-    //         </Link>
-    //       </div>
-    //     </form>
-    //   </div>
-    // </main>
-
-    <main>
-    <h1 className="text-4xl text-gray-700 text-center font-light mb-4 align-top pt-[5vh]">INSCRIPTION</h1>
-    <div className="flex flex-col items-center justify-center p-4 white">
-      <form className="space-y-4">
-        <div className="flex flex-col md:flex-row gap-2 auto-rows-auto">
-          <div className='flex items-center justify-center h-[40vh]'>
-            <div>
-               <label htmlFor="email" className="block text-sm font-medium text-gray-700">Adresse mail</label>
-               <input type="email" id="email" className="mt-1 p-2 w-full border-2 border-gray-300 rounded-md shadow-sm focus:border-indigo-500 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 text-gray-700" placeholder="mail@exemple.com" required />
-              <p className='h-[3vh]'>
-              </p>
-              <label htmlFor="password" className="block text-sm font-medium text-gray-700">Mot de passe</label>
-              <input type="password" id="password" className="mt-1 p-2 w-full border-2 border-gray-300 rounded-md shadow-sm focus:border-indigo-500 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 text-gray-700" required />
-              <p className='h-[3vh]'>
-              </p>
-              <label htmlFor="confirm-password" className="block text-sm font-medium text-gray-700">Confirmez le mot de passe</label>
-              <input type="password" id="confirm-password" className="mt-1 p-2 w-full border-2 border-gray-300 rounded-md shadow-sm focus:border-indigo-500 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 text-gray-700" required />
-              <p className='h-[3vh]'>
-              </p>
-              <div className="grid grid-cols-2 gap-4 items-center justify-center">
-                <button type="submit" className="px-4 py-2 bg-indigo-600 text-white rounded-md hover:bg-indigo-700 focus:outline-none focus:bg-indigo-700">S'inscrire</button>
-                <Link href={'/auth/login'}>
-                  <div className="text-sm text-indigo-600 hover:underline">Déjà un compte ? Se connecter</div>
-                </Link>
+    <main className="pb-20">
+      <h1 className="text-4xl text-gray-700 text-center font-light mb-4 align-top pt-20">INSCRIPTION</h1>
+      <div className="flex flex-col items-center justify-center p-4 white">
+        <form className="space-y-4">
+          <div className="flex flex-col md:flex-row gap-2 auto-rows-auto">
+            <div className='flex items-center justify-center h-[40vh]'>
+              <div>
+                <label htmlFor="email" className="block text-sm font-medium text-gray-700">Adresse mail</label>
+                <input type="email" id="email" className="mt-1 p-2 w-full border-2 border-gray-300 rounded-md shadow-sm focus:border-indigo-500 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 text-gray-700" placeholder="mail@exemple.com" required />
+                <p className='h-[3vh]'>
+                </p>
+                <label htmlFor="password" className="block text-sm font-medium text-gray-700">Mot de passe</label>
+                <input type="password" id="password" className="mt-1 p-2 w-full border-2 border-gray-300 rounded-md shadow-sm focus:border-indigo-500 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 text-gray-700" required />
+                <p className='h-[3vh]'>
+                </p>
+                <label htmlFor="confirm-password" className="block text-sm font-medium text-gray-700">Confirmez le mot de passe</label>
+                <input type="password" id="confirm-password" className="mt-1 p-2 w-full border-2 border-gray-300 rounded-md shadow-sm focus:border-indigo-500 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 text-gray-700" required />
+                <p className='h-[3vh]'>
+                </p>
+                <div className="grid grid-cols-2 gap-4 items-center justify-center">
+                  <button type="submit" className="px-4 py-2 bg-indigo-600 text-white rounded-md hover:bg-indigo-700 focus:outline-none focus:bg-indigo-700">S'inscrire</button>
+                  <Link href={'/auth/login'}>
+                    <div className="text-sm text-indigo-600 hover:underline">Déjà un compte ? Se connecter</div>
+                  </Link>
+                </div>
               </div>
             </div>
           </div>
-        </div>
-      </form>
-    </div>
+        </form>
+      </div>
     </main>
   );
 }
