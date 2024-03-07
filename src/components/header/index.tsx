ace6fad51c6f1cf964f9f16ca789edb8eced6b25
Thibault <t.morabire@gmail.com>
2024-03-07 16:32:28 +0100

    fix header

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -3,22 +3,22 @@ import React from "react";
 import Nav from "@/components/header/nav";
 import Banner from "@/components/header/banner";
 import MobileHeader from "@/components/header/mobile";
-import { useSmallScreen } from "@/hooks/useBreakpoint";
+import {useSmallScreen} from "@/hooks/useBreakpoint";
 
 function Header() {
-  const isSmallScreen = useSmallScreen();
-  return (
-    <>
-      {isSmallScreen ? (
-        <MobileHeader />
-      ) : (
+    const isSmallScreen = useSmallScreen();
+    return (
         <>
-          <Banner />
-          <Nav />
+            {isSmallScreen ? (
+                <MobileHeader/>
+            ) : (
+                <>
+                    <Banner/>
+                    <Nav/>
+                </>
+            )}
         </>
-      )}
-    </>
-  );
+    );
 }
 
 export default Header;
